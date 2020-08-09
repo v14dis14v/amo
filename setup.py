fa929b981a30082b336ac3b27cca37e8197f6053
v14dis14v <vladislav.reshetow@yandex.ru>
2020-08-09 22:03:38 +0300

    Добавляет файл установки пакета

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='amo',
+      version='0.1',
+      url='https://github.com/v14dis14v/amo',
+      license='MIT',
+      author='Vladislav Reshetov',
+      author_email='vladislav.reshetow@gmail.com',
+      description='Library for working with api amoCRM',
+      long_description=open('README.md').read(),
+      zip_safe=False)
