70cec175afc0b10e99e70a083c60c613c864e22e
v14dis14v <vladislav.reshetow@yandex.ru>
2020-08-11 14:04:39 +0300

    Добавляет рефакторинг

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='amo',
       author='Vladislav Reshetov',
       author_email='vladislav.reshetow@gmail.com',
       description='Library for working with api amoCRM',
-      install_requires=["requests;python_version='3.8'"],
+      install_requires=['requests'],
       long_description=open('README.md').read(),
       zip_safe=False)
